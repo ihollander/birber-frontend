18e698d0694fd926f84be209672eb43a940c1f28
ihollander <ianholla@gmail.com>
2021-04-15 13:59:43 -0400

    Implemented login feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import Header from "./Header";
 import Home from "./Home";
 import Login from "./Login";
 
 function App() {
+  const [user, setUser] = useState(null);
+  console.log(user);
+
+  function handleLogin(newUser) {
+    setUser(newUser);
+  }
+
+  // if the user is not logged in, show the login form
+  if (!user) {
+    return <Login onLogin={handleLogin} />;
+  }
+
+  // otherwise, show them the rest of the app
   return (
     <div className="App">
-      <Login />
       <Header />
       <Home />
     </div>
